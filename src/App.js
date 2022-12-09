bde43c9bcc204a66c259b606942e2f843489d04a
Uttam Rapeti <uttam.rapeti@leap360.com>
2022-12-09 20:52:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./style.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="">
+      <header className="py-3 bg-skyblue">
+        <div className="container">
+          <div className="flex items-center">
+            <div className="w-3/12">
+              <p className="logo font-semibold text-white">
+                Smart Error Detector
+              </p>
+            </div>
+            <div className="w-9/12"></div>
+          </div>
+        </div>
       </header>
+      <div className="">
+        <div className="flex h-full">
+          <div className="w-2/12 h-screen sidebar">Sidebar</div>
+          <div className="w-10/12">Detect Error App</div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        skyblue: "rgb(37, 112, 142)",
+      },
+    },
+  },
+  plugins: [],
+};
